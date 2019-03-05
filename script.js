3823ad6b053ec687d58c2652622cece18537daaf
Emily Wang <write2emily@hotmail.com>
2019-03-05 16:39:20 -0500

    css and array changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-     var array = [1,2,3,4, 5,6,7];
+     var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18, 19, 20, 21];
      var index =0;
 
  function start(){
@@ -11,29 +11,29 @@
 
  function shuffle(array) {
     var m = array.length, t, i;
-  
+
     // While there remain elements to shuffle…
     while (m) {
-  
+
       // Pick a remaining element…
       i = Math.floor(Math.random() * m--);
-  
+
       // And swap it with the current element.
       t = array[m];
       array[m] = array[i];
       array[i] = t;
     }
     return array;
-  }       
-  
+  }
+
 function call(){
 if (index < array.length){
         document.getElementById('beingCalled').innerText =array[index];
         var calledArray = [];
         for (var i =0; i< index; i++){
-          
+
             calledArray.unshift(array[i]);
-          
+
             document.getElementById('pastCalls').innerText =calledArray;
         }
         console.log(calledArray.length);
@@ -45,6 +45,3 @@ if (index < array.length){
         console.log("No more calls")
     }
 }
-   
-
-
